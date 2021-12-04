600d4789e52af9a495414d62c756ab277ef10cc6
Swan Desportes <swan.desportes@gmail.com>
2021-12-04 12:57:17 +0100

    remove config file from repo

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "xxx",
-    authDomain: "ohoho-20b14.firebaseapp.com",
-    projectId: "ohoho-20b14",
-    storageBucket: "ohoho-20b14.appspot.com",
-    messagingSenderId: "623929998942",
-    appId: "1:623929998942:web:d057429e2ea917fa813ecf",
-    measurementId: "G-MKYFEYT8EP"
-}
